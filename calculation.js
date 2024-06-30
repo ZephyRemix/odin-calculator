9b213b8a081005fb0bb6db5ce390a295eed7e985
ZephyRemix <hunyaochong@gmail.com>
2024-06-30 10:50:03 +0800

    Add calculation functions and declared variables to store numbers and operator

---
+++ calculation.js
@@ -0,0 +1,36 @@
+const num1 = 0;
+const num2 = 0;
+const operator = "";
+
+const divide = function(x, y) {
+	return x / y;
+};
+
+const multiply = function(x, y) {
+	return x * y;
+};
+
+const subtract = function(x, y) {
+	return x - y;  
+};
+
+const add = function(x, y) {
+	return x + y;
+};
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "/":
+            divide(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "+":
+            add(num1, num2);
+            break;
+    }
+}
