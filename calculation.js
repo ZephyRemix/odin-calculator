d7daf5d8ff2fde51624c899d0fc96ea37d3ac266
ZephyRemix <hunyaochong@gmail.com>
2024-06-30 20:16:33 +0800

    Add click event for operators

--- calculation.js
+++ calculation.js
@@ -1,6 +1,9 @@
-const num1 = 0;
-const num2 = 0;
-const operator = "";
+let num1 = 0;
+let num2 = 0;
+let registeredOperator = "";
+const display = document.querySelector(".display");
+const numList = document.querySelectorAll(".operand");
+const operatorList = document.querySelectorAll(".operator"); 
 
 const divide = function(x, y) {
 	return x / y;
@@ -35,9 +38,6 @@ function operate(operator, num1, num2) {
     }
 }
 
-const display = document.querySelector(".display");
-const numList = document.querySelectorAll(".operand");
-
 numList.forEach(num => num.addEventListener("click",    
     e => populateDisplay(e.target.textContent)));
 
@@ -48,3 +48,6 @@ function populateDisplay(num) {
         display.textContent = num;
     }
 }
+
+operatorList.forEach(operator => operator.addEventListener("click", 
+    e => console.log(registeredOperator = e.target.textContent)));
